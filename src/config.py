5b3169da2dcda4f948988dd58eb8efe2d9fd162a
Tian1975 <vcmky5ym84@privaterelay.appleid.com>
2026-07-13 09:50:56 +0200

    Add files via upload

--- src/config.py
+++ src/config.py
@@ -10,3 +10,9 @@ VERSION = "1.0.0"
 
 # Validació mínima de dades abans de desar un ticker com a vàlid
 MIN_SESSIONS = 200  # ~2 anys de sessions bursàtils sol donar ~500, marge ampli però segur
+
+# Llindar mínim d'èxit global perquè el job es consideri fallit de veritat.
+# Un o dos tickers puntuals (ex. ROG.SW sense dades un dia concret) són normals
+# i no han de fer caure el workflow. Un percentatge molt baix (com el 0% que
+# vam detectar amb el bug de festius desalineats) SÍ ha de fer-lo fallar.
+MIN_SUCCESS_RATE_PCT = 90.0
